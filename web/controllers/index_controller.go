9a7333b5be85371a5f7ef08313e6309a746bb42c
zhangli <842827855@qq.com>
2021-03-21 20:52:32 +0800

    feat: clear cache for distributed

--- web/controllers/index_controller.go
+++ web/controllers/index_controller.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"log"
+	"superstar/datasource"
+	"superstar/models"
 	"superstar/services"
 
 	"github.com/kataras/iris/v12/mvc"
@@ -49,5 +52,19 @@ func (c *IndexController) GetSearch() mvc.Result {
 			"Title":    "球星库",
 			"DataList": dataList,
 		},
+	}
+}
+
+// 集群多服务器的时候，才用得上这个接口
+// 性能优化的时候才考虑，加上本机的SQL缓存
+// http://localhost:8080/clearcache
+func (c *IndexController) GetClearcache() mvc.Result {
+	err := datasource.InstanceMaster().ClearCache(&models.StarInfo{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// set the model and render the view template.
+	return mvc.Response{
+		Text: "xorm缓存清除成功",
 	}
 }
